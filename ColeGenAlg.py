5cef8410b9851f16e1519da1185f0b4d145bc047
Cole Howard <cahowar9@ncsu.edu>
2023-11-26 00:32:52 -0500

    Completed GA Optimization Code to sort list

---
+++ ColeGenAlg.py
@@ -0,0 +1,60 @@
+import random
+
+def generate_list(length):
+    return [random.randint(1, length) for _ in range(length)]
+
+def generate_initial_population(population_size,list_length):
+    return [generate_list(list_length) for _ in range(population_size)]
+
+def fitness(lst):
+    total_sum = 0
+
+    for index, value in enumerate(lst):
+        difference = abs(value - index)
+        total_sum += difference
+
+    return total_sum
+
+def choose_parents(population,num_parents):
+    parents = sorted(population, key=fitness)[:num_parents]
+    return parents
+
+def crossover(parent1, parent2):
+    crossover_point = random.randint(1, len(parent1) - 1)
+    child = parent1[:crossover_point] + parent2[crossover_point:]
+    return child
+
+def mutate(child, mutation_rate):
+    for i in range(len(child)):
+        if random.random() < mutation_rate:
+            child[i] = random.randint(1, 100)
+    return child
+
+def evolve_population(population, generations, mutation_rate):
+    for generation in range(generations):
+        population.sort(key=fitness)
+        
+        if len(population) < 2:
+            continue  
+
+        parents = choose_parents(population, 2)
+        child = crossover(parents[0], parents[1])
+        child = mutate(child, mutation_rate)
+        population = population[:-1] 
+        population.append(child) 
+
+    population.sort(key=fitness)
+    return population
+
+def GA_sort(list_length, population_size, generations, mutation_rate):
+    population = generate_initial_population(population_size, list_length)
+    evolved_population = evolve_population(population, generations, mutation_rate)
+    return evolved_population[0]
+
+list_length = 10
+population_size = 1000
+generations = 10000
+mutation_rate = 0.1
+
+sorted_list = GA_sort(list_length, population_size, generations, mutation_rate)
+print("Sorted List:", sorted_list)
--- GenAlg.py
+++ GenAlg.py
@@ -7,14 +7,11 @@ def create_population(population_size, list_length):
     return [generate_random_list(list_length) for _ in range(population_size)]
 
 def calculate_fitness(lst):
-    # Fitness is calculated based on the number of correctly sorted pairs
-    return sum([1 for i in range(len(lst) - 1) if lst[i] <= lst[i + 1]])
+    # Fitness is calculated based on the number of incorrectly sorted pairs
+    return sum([1 for i in range(len(lst) - 1) if lst[i] > lst[i + 1]])
 
 def select_parents(population, num_parents):
-    parents = []
-    for _ in range(num_parents):
-        selected = random.choice(population)
-        parents.append(selected)
+    parents = sorted(population, key=calculate_fitness)[:num_parents]
     return parents
 
 def crossover(parent1, parent2):
@@ -30,15 +27,19 @@ def mutate(child, mutation_rate):
 
 def evolve_population(population, generations, mutation_rate):
     for generation in range(generations):
-        population.sort(key=calculate_fitness, reverse=True)
+        population.sort(key=calculate_fitness)
         
-        parents = select_parents(population, 6)
+        if len(population) < 2:
+            continue  # Skip this iteration if there are not enough individuals for crossover
+
+        parents = select_parents(population, 2)
         child = crossover(parents[0], parents[1])
         child = mutate(child, mutation_rate)
-        population[-1] = child
-    
+        population = population[:-1]  # Remove the least fit individual
+        population.append(child)  # Add the new child to the population
+
     # Final sorting
-    population.sort(key=calculate_fitness, reverse=True)
+    population.sort(key=calculate_fitness)
     return population
 
 def genetic_sort(list_length, population_size, generations, mutation_rate):
