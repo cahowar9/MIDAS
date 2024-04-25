181415b7636e951ded1f9b972992594b3e233804
Cole Howard <cahowar9@ncsu.edu>
2024-04-25 19:30:56 -0400

    edits

--- samples/sample_16/run.sh
+++ samples/sample_16/run.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -t 200:00:00                    # Walltime
 #SBATCH -N 1                           # Number of nodes
-#SBATCH -n 10                          # Number of processor cores (i.e. tasks)
+#SBATCH -n 4                          # Number of processor cores (i.e. tasks)
 #SBATCH -J "deap"               # Job name
 #SBATCH -p newq                      # Partition name
 #SBATCH -o output.txt                  # Output file name it will be printed in the $HOME directory by default
@@ -13,7 +13,7 @@ cd $SLURM_SUBMIT_DIR
 
 # Main -------------------------------------------------------------------------------------------------------------
 
-python ../../mofMain.py --input deap_list.yaml --cpus 10
+python ../../mofMain.py --input deap_list.yaml --cpus 4
 
 
 ##stress-ng --cpu $SLURM_CPUS_ON_NODE --timeout 60s --metrics-brief
