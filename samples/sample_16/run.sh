3e84ada8175cd2385574820395ef1da78fda1565
Cole Howard <cahowar9@ncsu.edu>
2024-04-25 18:48:39 -0400

    Change run file

--- samples/sample_16/run.sh
+++ samples/sample_16/run.sh
@@ -3,7 +3,7 @@
 #SBATCH -t 200:00:00                    # Walltime
 #SBATCH -N 1                           # Number of nodes
 #SBATCH -n 10                          # Number of processor cores (i.e. tasks)
-#SBATCH -J "parcs_lpo"               # Job name
+#SBATCH -J "deap"               # Job name
 #SBATCH -p newq                      # Partition name
 #SBATCH -o output.txt                  # Output file name it will be printed in the $HOME directory by default
 #SBATCH -e error.txt                   # Error file name it will be printed in the $HOME directory by default
@@ -14,7 +14,7 @@ cd $SLURM_SUBMIT_DIR
 # Main -------------------------------------------------------------------------------------------------------------
 
 rm -rf solution_*
-python ../../mofMain.py --input deap_list.yaml --cpus 4
+python ../../mofMain.py --input deap_list.yaml --cpus 10
 
 
 ##stress-ng --cpu $SLURM_CPUS_ON_NODE --timeout 60s --metrics-brief
