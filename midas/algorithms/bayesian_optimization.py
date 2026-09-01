593023ba0dc5481625494ef8de9794c2f31a1f06
cahowar9 <colehoward33@gmail.com>
2026-09-01 19:44:29 -0400

    bug fixes

--- midas/algorithms/bayesian_optimization.py
+++ midas/algorithms/bayesian_optimization.py
@@ -24,7 +24,6 @@ class Bayesian_Optimization:
 
     def __init__(self, input, noise=1e-6, random_state=None):
         self.input = input
-        self.dimensions = self.parse_dimensions()  # List of length D, each an array of categories
         self.population = []       # Stores categorical points
         self.fitness_values = []   # Stores fitness values
         self.random_state = np.random.RandomState(random_state) #Initialize random state used throughout problem
@@ -33,6 +32,7 @@ class Bayesian_Optimization:
         if input.calculation_type in ['single_cycle','eq_cycle']:
             self.binary_dims = [self.calculate_binary_length(len(dim_cats)) for dim_cats in self.dimensions]
             self.total_features = sum(self.binary_dims)  # Total number of binary features
+            self.dimensions = self.parse_dimensions()  # List of length D, each an array of categories
         else:
             self.total_features = int(max(value["index"] for value in self.input.genome.values())+1)
 
